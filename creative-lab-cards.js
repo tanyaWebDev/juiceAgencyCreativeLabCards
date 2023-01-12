8cb02ba4fe34366b46e0dc0390c5fdc228ec576b
tanyajuiceagency <117363514+tanyajuiceagency@users.noreply.github.com>
2023-01-12 16:23:45 +0700

    13 animation change resize

--- creative-lab-cards.js
+++ creative-lab-cards.js
@@ -529,72 +529,69 @@ function init() {
 
     /* THIRTEENTH CARDS 3D ANIMATION START */
 
+    var shouldRunAnimation = true;
     var thirteenthCard, thirteenthCardContent, thirteenthCards, dragDistancePerRotation, thirteenthRadius, thirteenthDiv, thirteenthProgressWrap, thirteenthSpin, startProgress;
 
     function applyLayout() {
         if (screen.width >= 992) {
-            thirteenthCard = document.querySelector(".thirteenth-cards__list");
-            thirteenthCardContent = document.querySelector(".three-d-card");
-            thirteenthCard.style.height = Math.ceil(thirteenthCardContent.offsetHeight / 3 * 2) + "px";
-            thirteenthCard.style.marginBottom = Math.ceil(thirteenthCardContent.offsetHeight / 3) + "px";
+            if (!thirteenthCards) {
+                shouldRunAnimation = true;
+                thirteenthCard = document.querySelector(".thirteenth-cards__list");
+                thirteenthCardContent = document.querySelector(".three-d-card");
+                thirteenthCard.style.height = Math.ceil(thirteenthCardContent.offsetHeight / 3 * 2) + "px";
+                thirteenthCard.style.marginBottom = Math.ceil(thirteenthCardContent.offsetHeight / 3) + "px";
 
-            thirteenthCards = gsap.utils.toArray(".three-d-card");
-            dragDistancePerRotation = 3000;
-            thirteenthRadius = 520;
-            thirteenthDiv = document.createElement("div");
-            thirteenthProgressWrap = gsap.utils.wrap(0, 1);
-            thirteenthSpin = gsap.fromTo(thirteenthCards, {
-                rotationY: i => i * 360 / thirteenthCards.length
-            }, {
-                rotationY: "-=360",
-                duration: 40,
-                ease: "none",
-                repeat: -1,
-                transformOrigin: "50% 50% " + -thirteenthRadius + "px"
-            });
-            startProgress = 0;
-            Draggable.create(thirteenthDiv, {
-                trigger: ".thirteenth-cards__list",
-                type: "x",
-                onPress() {
-                    gsap.killTweensOf(thirteenthSpin);
-                    thirteenthSpin.timeScale(0);
-                    startProgress = thirteenthSpin.progress();
-                },
-                onDrag: updateRotation,
-                onRelease() {
-                    gsap.to(thirteenthSpin, {
-                        timeScale: 1,
-                        duration: 1
-                    });
-                }
-            });
-            var intervalId = setInterval(function () {
-                if (screen.width < 992 && thirteenthSpin) {
-                    thirteenthSpin.kill();
-                    thirteenthCards = null;
-                    thirteenthDiv = null;
-                    thirteenthProgressWrap = null;
-                    clearInterval(intervalId);
-                }
-            }, 100);
+                thirteenthCards = gsap.utils.toArray(".three-d-card");
+                dragDistancePerRotation = 3000;
+                thirteenthRadius = 520;
+                thirteenthDiv = document.createElement("div");
+                thirteenthProgressWrap = gsap.utils.wrap(0, 1);
+                thirteenthSpin = gsap.fromTo(thirteenthCards, {
+                    rotationY: i => i * 360 / thirteenthCards.length
+                }, {
+                    rotationY: "-=360",
+                    duration: 40,
+                    ease: "none",
+                    repeat: -1,
+                    transformOrigin: "50% 50% " + -thirteenthRadius + "px"
+                });
+                startProgress = 0;
+                Draggable.create(thirteenthDiv, {
+                    trigger: ".thirteenth-cards__list",
+                    type: "x",
+                    onPress() {
+                        gsap.killTweensOf(thirteenthSpin);
+                        thirteenthSpin.timeScale(0);
+                        startProgress = thirteenthSpin.progress();
+                    },
+                    onDrag: updateRotation,
+                    onRelease() {
+                        gsap.to(thirteenthSpin, {
+                            timeScale: 1,
+                            duration: 1
+                        });
+                    }
+                });
+            }
+        } else {
+            shouldRunAnimation = false;
+            if (thirteenthCards) {
+                gsap.killTweensOf(thirteenthSpin);
+                thirteenthSpin = null;
+                thirteenthCards = null;
+                thirteenthDiv = null;
+                thirteenthProgressWrap = null;
+            }
         }
-    } else {
-        gsap.killTweensOf(thirteenthSpin);
-        thirteenthSpin = null;
-        thirteenthCards = null;
-        thirteenthDiv = null;
-        thirteenthProgressWrap = null;
     }
-}
 
-window.addEventListener("resize", applyLayout);
-applyLayout();
+    window.addEventListener("resize", applyLayout);
+    applyLayout();
 
-function updateRotation() {
-    let p = startProgress + (this.startX - this.x) / dragDistancePerRotation;
-    thirteenthSpin.progress(thirteenthProgressWrap(p));
-}
+    function updateRotation() {
+        let p = startProgress + (this.startX - this.x) / dragDistancePerRotation;
+        thirteenthSpin.progress(thirteenthProgressWrap(p));
+    }
 
     /* THIRTEENTH CARDS 3D ANIMATION END */
 
