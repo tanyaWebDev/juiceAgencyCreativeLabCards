2bd0535824fa238698e4afcfcbc9461d06207f1e
tanyajuiceagency <117363514+tanyajuiceagency@users.noreply.github.com>
2023-01-20 15:05:56 +0700

    added 12 and 13 animations

--- creative-lab-cards.js
+++ creative-lab-cards.js
@@ -370,170 +370,177 @@ function init() {
     }
     /* EIGHTH CARDS SCROLL ANIMATION END */
 
-    /* TWELFTH CAEDS DRAGGABLE ANIMATION START */ /*
-    
+    /* TWELFTH CAEDS DRAGGABLE ANIMATION START */
+
     gsap.registerPlugin(Draggable);
-    
-    let container = document.querySelector(".twelfth-cards__list");
-    let containerItems = document.querySelector(".twelfth-card__item");
-    let listItems = gsap.utils.toArray(".twelfth-card__item");
-    
-    let rowSize = container.offsetHeight - containerItems.offsetHeight * (containerItems.length / 3) - containerItems.length;
-    let colSize = container.offsetWidth - containerItems.offsetWidth * 2 - 2;
+
     let totalRows = 3;
-    let totalCols = 3;
-    
+    let gap = 16;
+    let rowSize = document.querySelector(".twelfth-cards__list").offsetWidth / totalRows;
+    let totalCols = Math.floor(document.querySelectorAll(".twelfth-card__item").length / totalRows);
+    let containerHeight = document.querySelector(".twelfth-card__item").offsetHeight * totalCols + (gap * (totalRows - 1)) + "px";
+    document.querySelector(".twelfth-cards__list").style.height = containerHeight;
+    let colSize = document.querySelector(".twelfth-cards__list").offsetWidth / totalCols;
+    let container = document.querySelector(".twelfth-cards__list");
+    let listItems = gsap.utils.toArray(".twelfth-card__item");
+
+
+
     let clampCol = gsap.utils.clamp(0, totalCols - 1);
     let clampRow = gsap.utils.clamp(0, totalRows - 1);
-    
+
     let cells = [];
-    
+
     // Map cell locations to array
     for (let row = 0; row < totalRows; row++) {
-    for (let col = 0; col < totalCols; col++) {
-        cells.push({
-            row: row,
-            col: col,
-            x: col * colSize,
-            y: row * rowSize
-        });
-    }
-    }
-    
-    let sortables = listItems.map(Sortable); // Array of sortables
-    let total = sortables.length;
-    
-    gsap.to(container, { autoAlpha: 1, duration: 0.5 });
-    
-    function changeIndex(item, to, sameRow, sameCol) {
-    
-    // Check if adjacent to new position
-    if ((sameRow && !sameCol) || (!sameRow && sameCol)) {
-    
-        // Swap positions in array
-        var temp = sortables[to];
-        sortables[to] = item;
-        sortables[item.index] = temp;
-    
-    } else {
-    
-        // Change position in array
-        arrayMove(sortables, item.index, to);
-    }
-    
-    // Simple, but not optimized way to change element's position in DOM. Not always necessary. 
-    sortables.forEach(sortable => container.appendChild(sortable.element));
-    
-    // Set index for each sortable
-    sortables.forEach((sortable, index) => sortable.setIndex(index));
-    }
-    
-    function Sortable(element, index) {
-    
-    let content = element.querySelector(".twelfth-card__item-content");
-    let order = element.querySelector(".twelfth-card__item-number");
-    
-    let animation = gsap.to(content, {
-        duration: 0.3,
-        boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
-        force3D: true,
-        scale: 1.1,
-        paused: true
-    });
-    
-    let dragger = new Draggable(element, {
-        onDragStart: downAction,
-        onRelease: upAction,
-        onDrag: dragAction,
-        cursor: "inherit"
-    });
-    
-    // let position = element._gsTransform;
-    let getProp = gsap.getProperty(element);
-    
-    // Public properties and methods
-    let sortable = {
-        cell: cells[index],
-        dragger: dragger,
-        element: element,
-        index: index,
-        setIndex: setIndex
-    };
-    
-    gsap.set(element, {
-        x: sortable.cell.x,
-        y: sortable.cell.y,
-    });
-    
-    function setIndex(index) {
-    
-        let cell = cells[index];
-        // var dirty = position.x !== cell.x || position.y !== cell.y;
-        let dirty = getProp("x") !== cell.x || getProp("y") !== cell.y;
-    
-        sortable.cell = cell;
-        sortable.index = index;
-        order.textContent = index + 1;
-    
-        // Don't layout if you're dragging
-        if (!dragger.isDragging && dirty) layout();
-    }
-    
-    function downAction() {
-        animation.play();
-        this.update();
-    }
-    
-    function dragAction() {
-    
-        let col = clampCol(Math.round(this.x / colSize));
-        let row = clampRow(Math.round(this.y / rowSize));
-    
-        let cell = sortable.cell;
-        let sameCol = col === cell.col;
-        let sameRow = row === cell.row;
-    
-        // Check if position has changed
-        if (!sameRow || !sameCol) {
-    
-            // Calculate the new index
-            var index = totalCols * row + col;
-    
-            // Update the model
-            changeIndex(sortable, index, sameRow, sameCol);
+        for (let col = 0; col < totalCols; col++) {
+            cells.push({
+                row: row,
+                col: col,
+                x: col * (document.querySelector(".twelfth-cards__list").offsetWidth / totalRows),
+                y: row * ((document.querySelector(".twelfth-cards__list").offsetHeight + gap) / totalRows),
+            });
         }
     }
-    
-    function upAction() {
-        animation.reverse();
-        layout();
+
+    let sortables = listItems.map(Sortable); // Array of sortables
+    let total = sortables.length;
+
+    gsap.to(container, { autoAlpha: 1, duration: 0.5 });
+
+    function changeIndex(item, to, sameRow, sameCol) {
+
+        // Check if adjacent to new position
+        if ((sameRow && !sameCol) || (!sameRow && sameCol)) {
+
+            // Swap positions in array
+            var temp = sortables[to];
+            sortables[to] = item;
+            sortables[item.index] = temp;
+
+        } else {
+
+            // Change position in array
+            arrayMove(sortables, item.index, to);
+        }
+
+        // Simple, but not optimized way to change element's position in DOM. Not always necessary. 
+        sortables.forEach(sortable => container.appendChild(sortable.element));
+
+        // Set index for each sortable
+        sortables.forEach((sortable, index) => sortable.setIndex(index));
     }
-    
-    function layout() {
-        gsap.to(element, {
+
+    function Sortable(element, index) {
+
+        let content = element.querySelector(".twelfth-card__item-content");
+        let order = element.querySelector(".twelfth-card__item-number");
+
+        let animation = gsap.to(content, {
             duration: 0.3,
-            x: sortable.cell.x,
-            y: sortable.cell.y
+            boxShadow: "rgba(0,0,0,0.2) 0px 16px 32px 0px",
+            force3D: true,
+            scale: 1.1,
+            paused: true
         });
+
+        let dragger = new Draggable(element, {
+            onDragStart: downAction,
+            onRelease: upAction,
+            onDrag: dragAction,
+            cursor: "inherit"
+        });
+
+        // let position = element._gsTransform;
+        let getProp = gsap.getProperty(element);
+
+        // Public properties and methods
+        let sortable = {
+            cell: cells[index],
+            dragger: dragger,
+            element: element,
+            index: index,
+            setIndex: setIndex
+        };
+
+        gsap.set(element, {
+            x: sortable.cell.x,
+            y: sortable.cell.y,
+        });
+
+        function setIndex(index) {
+
+            let cell = cells[index];
+            // var dirty = position.x !== cell.x || position.y !== cell.y;
+            let dirty = getProp("x") !== cell.x || getProp("y") !== cell.y;
+
+            sortable.cell = cell;
+            sortable.index = index;
+            order.textContent = index + 1;
+
+            // Don't layout if you're dragging
+            if (!dragger.isDragging && dirty) layout();
+        }
+
+        function downAction() {
+            animation.play();
+            this.update();
+        }
+
+        function dragAction() {
+
+            let col = clampCol(Math.round(this.x / colSize));
+            let row = clampRow(Math.round(this.y / rowSize));
+
+            let cell = sortable.cell;
+            let sameCol = col === cell.col;
+            let sameRow = row === cell.row;
+
+            // Check if position has changed
+            if (!sameRow || !sameCol) {
+
+                // Calculate the new index
+                var index = totalCols * row + col;
+
+                // Update the model
+                changeIndex(sortable, index, sameRow, sameCol);
+            }
+        }
+
+        function upAction() {
+            animation.reverse();
+            layout();
+        }
+
+        function layout() {
+            gsap.to(element, {
+                duration: 0.3,
+                x: sortable.cell.x,
+                y: sortable.cell.y
+            });
+        }
+
+        return sortable;
     }
-    
-    return sortable;
-    }
-    
+
     // Changes an elements's position in array
     function arrayMove(array, from, to) {
-    array.splice(to, 0, array.splice(from, 1)[0]);
-    } */
+        array.splice(to, 0, array.splice(from, 1)[0]);
+    }
 
-    /* TWELFTH CARDS DRAGGABLE ANIMATION END */
+    /* TWELFTH CAEDS DRAGGABLE ANIMATION END */
 
     /* THIRTEENTH CARDS 3D ANIMATION START */
 
-   /* var shouldRunAnimation = true;
+    var shouldRunAnimation = true;
     var thirteenthCard, thirteenthCardContent, thirteenthCards, dragDistancePerRotation, thirteenthRadius, thirteenthDiv, thirteenthProgressWrap, thirteenthSpin, startProgress;
+
+    window.addEventListener("resize", applyLayout);
+    applyLayout();
 
     function applyLayout() {
         if (screen.width >= 992) {
+            // Initialize animation and draggable functionality for the cards
             if (!thirteenthCards) {
                 shouldRunAnimation = true;
                 thirteenthCard = document.querySelector(".thirteenth-cards__list");
@@ -575,6 +582,7 @@ function init() {
             }
         } else {
             shouldRunAnimation = false;
+            // Remove animation and draggable functionality for the cards
             if (thirteenthCards) {
                 gsap.killTweensOf(thirteenthSpin);
                 thirteenthSpin.timeScale(0);
@@ -587,13 +595,10 @@ function init() {
         }
     }
 
-    window.addEventListener("resize", applyLayout);
-    applyLayout();
-
     function updateRotation() {
         let p = startProgress + (this.startX - this.x) / dragDistancePerRotation;
         thirteenthSpin.progress(thirteenthProgressWrap(p));
-    } */
+    }
 
     /* THIRTEENTH CARDS 3D ANIMATION END */
 
